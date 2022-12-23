04d5e488667dfd4bf10b9bf6a906baaf84dbcb40
Thiagonox <thiagogentil499@gmail.com>
2022-12-23 12:48:03 -0300

    feat pagina blog

---
+++ components/chip.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="snap-center p-3 mx-2 border border-white/50 rounded-full flex items-center bg-[#45483F]">
+        <p class="text-text text-xl font-principal">Planta</p>
+    </div>
+</template>
--- pages/blog.vue
+++ pages/blog.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>oi</h1>
+    <div class="bg-principal pt-6">
+        <main class="w-11/12 m-auto">
+            <h1 class="ml-6 text-text text-6xl font-principal">
+              Artigos
+            </h1>
+            <div class="snap-y snap-mandatory overflow-scroll scrollbar-hide w-full h-10 flex my-4">
+                <Chip></Chip>
+                <Chip></Chip>
+                <Chip></Chip>
+                <Chip></Chip>
+                <Chip></Chip>
+                <Chip></Chip>
+            </div>
+            <Article></Article>
+            <Article></Article>
+            <Article></Article>
+            <Article></Article>
+            <Article></Article>
+            <Article></Article>
+        </main>
+    </div>
 </template>
