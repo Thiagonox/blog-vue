8e6c1203309f5d030f4767a5d0a9ae3a77a736ce
Thiagonox <thiagogentil499@gmail.com>
2022-12-23 22:31:00 -0300

    refactor

--- components/footer.vue
+++ components/footer.vue
@@ -1,10 +1,10 @@
 <template>
     <footer class="w-full h-20 bg-principal">
-        <div class="flex flex-row items-center">
+        <div class="flex flex-row items-center mb-4">
             <h1 class="font-secundaria text-xl"><mark class="bg-[#E5E3DD] py-2 px-1">FRAME</mark></h1>
             <hr class="w-full h-1 border-tertiary">
         </div>
-        <div class="w-full flex items-center justify-center">
+        <div class="w-full flex items-center justify-center mb-6">
             <h2 class="text-text text-lg font-secundaria">Copyright 2022 - Todos os direitos reservados</h2>
         </div>
     </footer>
--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,10 @@
         <header></header>
         <main class="">
             <div class="w-full m-auto pt-4 px-2">
-                <h1 class="absolute left-6 top-24 text-text text-5xl font-principal">
+                <h1 class="md:w-1/2 absolute ml-6 mt-6 text-text text-5xl font-principal">
                     Um blog para quem gosta de plantas e decoração
                 </h1>
-                <img class="w-full rounded-3xl" src="https://photo-cdn2.icons8.com/_yZJWIh2uk3UDtFzvPrapRNHPJxnI8PBXwCVVHm2Pu8/rs:fit:1608:1072/czM6Ly9pY29uczgu/bW9vc2UtcHJvZC5l/eHRlcm5hbC9hMmE0/Mi83YjExYzc0MGYz/MWY0NzgwOTdjNGY0/Nzc0NmU0YWI4MC5q/cGc.jpg" alt="">
+                <img class="w-full md:h-60 md:object-cover rounded-3xl" src="https://photo-cdn2.icons8.com/_yZJWIh2uk3UDtFzvPrapRNHPJxnI8PBXwCVVHm2Pu8/rs:fit:1608:1072/czM6Ly9pY29uczgu/bW9vc2UtcHJvZC5l/eHRlcm5hbC9hMmE0/Mi83YjExYzc0MGYz/MWY0NzgwOTdjNGY0/Nzc0NmU0YWI4MC5q/cGc.jpg" alt="">
             </div>
             <div class="w-11/12 mx-auto mt-6">
                 <h2 class="ml-6 text-text text-5xl font-principal">
@@ -16,8 +16,6 @@
                     <Article></Article>
                     <Article></Article>
                     <Article></Article>
-                    <button class="w-10 h-10 bg-white" @click="created"></button>
-                    <p>{{ titulos }}</p>
                 </div>
             </div>
         </main>
