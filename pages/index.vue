01fa70532d7a7d3436b7d7653137d93a3fc80cd8
Thiagonox <thiagogentil499@gmail.com>
2022-12-26 15:51:01 -0300

    google ads teste

--- pages/index.vue
+++ pages/index.vue
@@ -65,3 +65,15 @@ export default {
 async function oi(){src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5673700186739436"
      crossorigin="anonymous"}
 </script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5673700186739436"
+     crossorigin="anonymous"></script>
+<!-- vertical -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5673700186739436"
+     data-ad-slot="2926141157"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
