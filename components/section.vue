53aafdba9ed60653a616a823c18b272383991750
Thiagonox <thiagogentil499@gmail.com>
2022-12-26 20:22:08 -0300

    Revert "refactor section"
    This reverts commit cbaa6c3b095b5a4a0a77276139eab04feab6b177.

--- components/section.vue
+++ components/section.vue
@@ -2,3 +2,15 @@
     <section class="h-full ml-4">
     </section>
 </template>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5673700186739436"
+     crossorigin="anonymous"></script>
+<!-- vertical -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5673700186739436"
+     data-ad-slot="2926141157"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
