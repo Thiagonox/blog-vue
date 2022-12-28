cdb25bae4bb2a75e004210cb63032d4d24269ceb
Thiagonox <thiagogentil499@gmail.com>
2022-12-27 21:47:44 -0300

    refactor navbar

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,12 +1,13 @@
 <template>
     <section class="bg-[#232420]">
-        <nav class="h-screen flex flex-col items-center ">
-            <ul class="w-full h-1/5 flex flex-col items-center justify-between pt-3 text-text font-principal">
-                <li class="p-2 rounded-xl" v-bind:class="{'bg-[#2B4C4A]': $route.path === '/'}"><router-link class="w-full" to="/"><img class="w-7" src="../assets/img/home.svg" alt="" srcset=""></router-link></li>
-                <li class="p-2 rounded-xl" v-bind:class="{'bg-[#2B4C4A]': $route.path === '/blog'}"><router-link class="w-full" to="/blog"><img class="w-7" src="../assets/img/pages.svg" alt="" srcset=""></router-link></li>
+        <nav class="h-screen flex flex-col items-center justify-between py-3">
+            <ul class="w-full h-48 flex flex-col items-center justify-between text-text font-principal">
+                <li class="p-2 rounded-xl" v-bind:class="{'bg-[#324F10]': $route.path === '/'}"><router-link class="w-full" to="/"><img class="w-7" src="../assets/img/home.svg" alt="" srcset=""></router-link></li>
+                <li class="p-2 rounded-xl" v-bind:class="{'bg-[#324F10]': $route.path === '/blog'}"><router-link class="w-full" to="/blog"><img class="w-7" src="../assets/img/pages.svg" alt="" srcset=""></router-link></li>
+                <li class="p-2 rounded-xl" v-bind:class="{'bg-[#324F10]': $route.path === '/blog'}"><router-link class="w-full" to="/blog"><img class="w-7" src="../assets/img/pages.svg" alt="" srcset=""></router-link></li>
             </ul>
-            <button class="m-auto w-10 h-10 rounded-full hover:bg-tertiary active:bg-[#33342F]">
-                    <img class="w-7 m-auto" src="../assets/img/light_mode.svg" alt="">
+            <button class="mx-auto w-12 h-12 border border-white/40 rounded-full hover:bg-tertiary active:bg-[#33342F]">
+                    <img class="w-6 m-auto" src="../assets/img/light_mode.svg" alt="">
             </button>
         </nav>
     </section>
