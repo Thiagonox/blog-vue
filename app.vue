5370834fbc3cf20884bc07dea133239ca10614cd
Thiagonox <thiagogentil499@gmail.com>
2022-12-27 21:04:43 -0300

    refactor navigation

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="md:flex">
-        <Navigation class="hidden md:block w-[5%]" />
-        <div class=" md:w-[95%]">
+        <Navigation class="hidden md:block md:fixed w-[5%]" />
+        <div class="md:ml-[5%] md:w-[95%]">
             <Header />
             <NuxtPage />
             <Footer />
--- components/navigation.vue
+++ components/navigation.vue
@@ -1,10 +1,13 @@
 <template>
-    <section class="bg-[#23261E]">
-        <nav class="h-screen ">
-            <ul class="w-full h-full flex flex-col items-center justify-around">
-                <li class="p-2 rounded-xl" v-bind:class="{'bg-tertiary': $route.path === '/'}"><router-link class="w-full" to="/"><img class="w-8" src="../assets/img/home.svg" alt="" srcset=""></router-link></li>
-                <li class="" v-bind:class="{'bg-tertiary': $route.path === '/blog'}"><router-link class="w-full" to="/blog"><img class="w-8 mr-2" src="../assets/img/pages.svg" alt="" srcset=""></router-link></li>
+    <section class="bg-[#232420]">
+        <nav class="h-screen flex flex-col items-center ">
+            <ul class="w-full h-1/5 flex flex-col items-center justify-between pt-3 text-text font-principal">
+                <li class="p-2 rounded-xl" v-bind:class="{'bg-[#2B4C4A]': $route.path === '/'}"><router-link class="w-full" to="/"><img class="w-7" src="../assets/img/home.svg" alt="" srcset=""></router-link></li>
+                <li class="p-2 rounded-xl" v-bind:class="{'bg-[#2B4C4A]': $route.path === '/blog'}"><router-link class="w-full" to="/blog"><img class="w-7" src="../assets/img/pages.svg" alt="" srcset=""></router-link></li>
             </ul>
+            <button class="m-auto w-10 h-10 rounded-full hover:bg-tertiary active:bg-[#33342F]">
+                    <img class="w-7 m-auto" src="../assets/img/light_mode.svg" alt="">
+            </button>
         </nav>
     </section>
 </template>
