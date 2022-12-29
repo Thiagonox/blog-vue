0e177bd384b4a401bebd1b4e6df026f31ab1b6f1
Thiagonox <thiagogentil499@gmail.com>
2022-12-28 21:32:50 -0300

    feat rotas dinamicas

--- components/article.vue
+++ components/article.vue
@@ -3,16 +3,19 @@
         <h1 class="text-text text-3xl font-principal mb-2 truncate">{{ titulo }}</h1>
         <h2 class="text-text/50 text-lg font-secundaria mb-2">{{ tag }}</h2>
         <p class="h-28 mb-6 text-text text-xl font-principal overflow-hidden text-ellipsis">{{ resumo }}</p>
-        <a href="{{ artigo }}"><img class="w-6" src="../assets/img/open.svg" alt=""></a>
+        <nuxt-link :to="`/${href}`"><img class="w-6" src="../assets/img/open.svg" alt="">
+        </nuxt-link>
     </div>
 </template>
 <script>
+
 export default {
+  name: 'Article',
   props: {
     titulo: String,
     tag: String,
     resumo: String,
-    artigo: String
+    href: String
   }
 }
 </script>
--- pages/blog.vue
+++ pages/blog.vue
@@ -7,15 +7,18 @@
             <div class="snap-y snap-mandatory overflow-scroll scrollbar-hide w-full h-10 flex my-4">
                 <Chip v-for="artigo in listaArtigo.slice(0, 10)" :chip="artigo.tag"/>
             </div>
-            <div id="artigos" class="">
-                    <Article v-for="artigo in listaArtigo.slice(0, 10)" :titulo="artigo.titulo" :tag="artigo.tag" :resumo="artigo.resumo"/>
-            </div>
+            <div class=" mt-6 grid md:grid-cols-4 gap-4">
+                    <div class="md:col-span-2">
+                        <Article v-for="artigo in listaArtigo.slice(0, 3)" :titulo="artigo.titulo" :tag="artigo.tag" :resumo="artigo.resumo" :href="artigo.id.toString()" :key="artigo.id"/>
+                    </div>
+                    <div class="hidden md:block md:col-span-2">
+                        <Article v-for="artigo in listaArtigo.slice(3, 6)" :titulo="artigo.titulo" :tag="artigo.tag" :href="artigo.id.toString()" :key="artigo.id"/>
+                    </div>
+                </div>
         </main>
     </div>
 </template>
 <script>
-
-import Article from '../components/article.vue'
 
 import axios from 'axios'
 
@@ -40,7 +43,7 @@ export default {
         ]
       }
     },
-    created() {
+    async asyncData() {
       axios.interceptors.request.use(function (config) {
           config.headers['apikey'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhremluZHFpanV2cnFmcWh0ZmR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzAxNjE3NzQsImV4cCI6MTk4NTczNzc3NH0.Pmr3Q3-bA26YyddjqkD0gG9w4laU9YJCYjSrUoEv03Q';
           config.headers['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhremluZHFpanV2cnFmcWh0ZmR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzAxNjE3NzQsImV4cCI6MTk4NTczNzc3NH0.Pmr3Q3-bA26YyddjqkD0gG9w4laU9YJCYjSrUoEv03Q';
