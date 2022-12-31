99938d4b3d7f17d91feb1c0a7b7c05e20a48b82e
Thiagonox <thiagogentil499@gmail.com>
2022-12-30 22:07:20 -0300

    feat categories to index

--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,14 @@
               </div>
               <img class="w-full md:h-60 md:object-cover rounded-3xl" src="https://photo-cdn2.icons8.com/_yZJWIh2uk3UDtFzvPrapRNHPJxnI8PBXwCVVHm2Pu8/rs:fit:1608:1072/czM6Ly9pY29uczgu/bW9vc2UtcHJvZC5l/eHRlcm5hbC9hMmE0/Mi83YjExYzc0MGYz/MWY0NzgwOTdjNGY0/Nzc0NmU0YWI4MC5q/cGc.jpg" alt="">
             </div>
+            <div class="w-11/12 m-auto">
+              <div class="my-6">
+                <h2 class="mt-6 text-text text-5xl font-principal">
+                  Categorias
+                </h2>
+              </div>
+              <Categories></Categories>
+            </div>
             <div class="w-11/12 mx-auto">
                 <h2 class="mt-6 text-text text-5xl font-principal">
                     Artigos
