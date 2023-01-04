8e5c11b93d45bd5c33ee52732fdca297b7c052f8
Thiagonox <thiagogentil499@gmail.com>
2023-01-04 20:56:36 -0300

    refactor

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="md:flex">
+    <div class="w-screen md:flex">
         <Navigation class="hidden md:block md:fixed w-20" />
         <div class="md:ml-20 w-full">
             <Header />
