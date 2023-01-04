a3b873bc71f2c0abc1f710077e300aee8a4aa1af
Thiagonox <thiagogentil499@gmail.com>
2023-01-04 20:57:20 -0300

    fix: Article overflow

--- components/article.vue
+++ components/article.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class=" rounded-2xl mt-4 p-6 bg-[#23261E] hover:bg-[#3E4A36] duration-200 overflow-hidden text-ellipsis">
+    <div class="w-full rounded-2xl mt-4 p-6 bg-[#23261E] hover:bg-[#3E4A36] duration-200 overflow-hidden text-ellipsis">
         <h1 class="text-text text-3xl font-principal mb-2 truncate">{{ titulo }}</h1>
         <h2 v-for="tag in tags" class="text-text/50 text-lg font-secundaria mb-2">{{ tag }}</h2>
-        <p class="h-28 mb-6 text-text text-xl font-principal overflow-hidden text-ellipsis">{{ resumo }}</p>
+        <p class="w-full h-28 mb-6 text-text text-xl font-principal overflow-hidden text-ellipsis">{{ resumo }}</p>
         <nuxt-link :to="`/${link}`"><img class="w-6" src="../assets/img/open.svg" alt="">
         </nuxt-link>
     </div>
