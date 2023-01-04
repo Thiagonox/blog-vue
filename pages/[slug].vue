21ee7937d4fa259695b4b0545b1bc6612873f12f
Thiagonox <thiagogentil499@gmail.com>
2023-01-04 17:58:47 -0300

    feat: route recovery on dynamic page

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -6,32 +6,30 @@
                 
                 <h1 class="-mt-20 text-5xl font-principal">
                     <mark class="bg-text">
-                       {{ titulo }}
+                       {{ post.titulo }}
                     </mark>
                 </h1>
             </div>
-            <h2 class="text-text/50 text-lg font-secundaria mb-6">{{ tag }}</h2>
+            <h2 class="text-text/50 text-lg font-secundaria mb-6">{{ post.tag }}</h2>
             <h2 class="text-text text-2xl font-secundaria mb-6"><b>Escolha as plantas certas para o seu jardim</b></h2>
             <p class="text-text text-xl font-principal mb-6">Antes de comprar qualquer planta, é importante considerar o clima e o solo em que ela será plantada. Algumas plantas só prosperam em áreas ensolaradas, enquanto outras preferem sombra. Também é importante considerar o tamanho e a forma que a planta terá no jardim, bem como sua necessidade de cuidados.</p>
-            <h2 class="text-text text-2xl font-secundaria mb-6"><b>Cuide bem das suas plantas</b></h2>
-            <p class="text-text text-xl font-principal mb-6">As plantas de jardim precisam de água, solo fértil e exposição adequada ao sol para crescer fortes e saudáveis. Certifique-se de seguir as recomendações de cuidado específicas para cada espécie para garantir que elas recebam o que precisam para prosperar.
-            </p>
-            <h2 class="text-text text-2xl font-secundaria mb-6"><b>Crie um ambiente atraente</b></h2>
-            <p class="text-text text-xl font-principal mb-6">As plantas de jardim são uma excelente maneira de adicionar cor e beleza ao seu ambiente. Considere combinar plantas de diferentes tamanhos e cores para criar um visual atraente. Você também pode adicionar elementos decorativos, como bancos, fontes ou esculturas, para dar personalidade ao seu jardim.</p>
-            <h2 class="text-text text-2xl font-secundaria mb-6"><b>Aproveite o seu jardim</b></h2>
-            <p class="text-text text-xl font-principal">Não deixe que o seu jardim se torne apenas um lugar para olhar. Aproveite o espaço para relaxar, ler um livro ou fazer um piquenique. Adicione mobiliário de jardim confortável e aproveite o ar fresco e a natureza ao seu redor.</p>
+            
         </main>
     </div>
 </template>
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
-      titulo: '',
-      conteudo: ''
+      post: [],
     }
   },
-  async asyncData({ params }) {
-  }
+  async created() {
+    
+    const response = await axios.get(`https://d0xguy57.directus.app/items/Post${this.$route.path}`);
+    this.post = response.data.data;
+  },
 }
 </script>
