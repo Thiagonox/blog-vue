081b815d7796dd7b995e40fd98777b96e0d19155
Thiagonox <thiagogentil499@gmail.com>
2023-01-04 21:20:28 -0300

    refactor: paragraph

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -4,7 +4,7 @@
             <div class="w-full m-auto mb-10">
                 <img class="w-full md:h-60 md:object-cover rounded-3xl" src="https://photo-cdn2.icons8.com/0dh5qMhiZOUyICiSo6i_ZZ9zjKsqIsl0QvfSGUp7tN4/rs:fit:1608:1072/czM6Ly9pY29uczgu/bW9vc2UtcHJvZC5l/eHRlcm5hbC9hMmE0/Mi8xNDY4ZDQyNDBh/YjM0MmUzODNjY2M0/ZTI0YzkxNTg1Mi5q/cGc.jpg" alt="">
                 
-                <h1 class="-mt-20 text-5xl font-principal">
+                <h1 class="-mt-20 text-4xl font-principal">
                     <mark class="bg-text">
                        {{ post.titulo }}
                     </mark>
@@ -14,8 +14,8 @@
               <h2  v-for="tag in post.tag" class="text-text/50 text-lg font-secundaria ml-4">{{ tag }}</h2>
             </div>
             <div class="md:w-3/5">
-              <h2 class="text-text text-2xl font-secundaria mb-6"><b>{{ post.resumo }}</b></h2>
-              <p class=" text-text text-xl font-principal mb-6">{{ post.conteudo1 }}</p>
+              <h2 class="text-text text-xl font-secundaria mb-6"><b>{{ post.resumo }}</b></h2>
+              <p class=" text-text text-xl leading-7 font-principal mb-6">{{ post.conteudo1 }}</p>
             </div>
         </main>
     </div>
