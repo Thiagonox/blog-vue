b48d906b9e9a45048f352835df11ae9a86d22682
Thiagonox <thiagogentil499@gmail.com>
2023-01-06 11:11:49 -0300

    adsense code

--- app.vue
+++ app.vue
@@ -8,3 +8,5 @@
         </div>
     </div>
 </template>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5673700186739436"
+     crossorigin="anonymous"></script>
