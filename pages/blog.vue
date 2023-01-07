a6966c58df504142b8be331d57e9422c04aa3c00
Thiagonox <thiagogentil499@gmail.com>
2023-01-06 21:24:32 -0300

    style: grid

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,19 +1,22 @@
 <template>
     <div class="bg-principal pt-6">
         <main class="w-11/12 m-auto">
-            <h1 class="ml-6 text-text text-6xl font-principal">
+            <h1 class="ml-6 text-text text-8xl font-principal">
               Artigos
             </h1>
             <div class="snap-y snap-mandatory overflow-scroll scrollbar-hide w-full h-10 flex my-4">
                 <Chip v-for=" artigo in listaArtigo.slice(0, 10)" :chip="artigo.tag"/>
             </div>
-            <div class="w-full mt-6 grid md:grid-cols-4 gap-4">
-                    <div class="md:col-span-2">
-                        <Article v-for="artigo in listaArtigo.slice(0, 5)" :titulo="artigo.titulo" :tag="artigo.tag" :resumo="artigo.resumo" :link="artigo.id.toString()"/>
-                    </div>
-                    <div class="hidden md:block md:col-span-2">
-                        <Article v-for="artigo in listaArtigo.slice(2, 5)" :titulo="artigo.titulo" :tag="artigo.tag" :resumo="artigo.resumo" :link="artigo.id.toString()"/>
-                    </div>
+            <div class="w-full mt-6 md:grid md:grid-cols-8 md:gap-8">
+              <div class="md:col-span-3">
+                  <Article v-for="artigo in listaArtigo.slice(0, 5)" :titulo="artigo.titulo" :tag="artigo.tag" :resumo="artigo.resumo" :link="artigo.id.toString()"/>
+              </div>
+              <div class="hidden md:block md:col-span-3">
+                  <Article v-for="artigo in listaArtigo.slice(5, 10)" :titulo="artigo.titulo" :tag="artigo.tag" :resumo="artigo.resumo" :link="artigo.id.toString()"/>
+              </div>
+              <div class="h-full mt-3 rounded-2xl bg-[#23261E] hidden md:block md:col-span-2">
+                  
+              </div>
             </div>
         </main>
     </div>
